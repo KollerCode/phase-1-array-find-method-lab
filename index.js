7d67e27e336a44d40968fb770612fb70cbb68bbe
Natasha Koller <kollernatasha@gmail.com>
2021-12-12 21:30:55 -0500

    commit tree d683bb5e5edf63799c8d6c9701afa1c9fd93c576

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+function superbowlWin(record) {
+    let result = record.find(function(object) {
+        return object.result === 'W'
+    })
+    if(result){
+        return result.year
+    } else {
+        return result
+    }
+}
 
+// 
+// superbowlWin = (record) => {
+//     const result = record.find( record => record.result === "W" );
+//     return !!result ? result.year : undefined;
+//   }
